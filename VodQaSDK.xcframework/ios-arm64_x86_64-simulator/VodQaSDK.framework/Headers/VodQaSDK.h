039e94078abd1daa9151246d22d331b28a965bda
YousefAbuJalboush <yousef.y.jalboush@gmail.com>
2025-05-26 17:11:24 +0300

    Add xcframework

---
+++ VodQaSDK.xcframework/ios-arm64_x86_64-simulator/VodQaSDK.framework/Headers/VodQaSDK.h
@@ -0,0 +1,18 @@
+//
+//  VodQaSDK.h
+//  VodQaSDK
+//
+//  Created by Yousef Jalboush on 11/05/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VodQaSDK.
+FOUNDATION_EXPORT double VodQaSDKVersionNumber;
+
+//! Project version string for VodQaSDK.
+FOUNDATION_EXPORT const unsigned char VodQaSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VodQaSDK/PublicHeader.h>
+
+
